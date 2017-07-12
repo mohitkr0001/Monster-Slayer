d4ee57d35503bdfd3895a12545bd2f56376c2c10
Mohit Kumar Singh <mohitkr0231@gmail.com>
2017-07-12 20:21:55 +0530

    Final for review

---
+++ app.js
@@ -0,0 +1,78 @@
+new Vue({
+  el:'#game',
+  data:{
+    playerHealth:60,
+    monsterHealth:80,
+    isGameStarted:false,
+    count:0
+  },
+  methods:{
+    startGame:function(){
+      this.isGameStarted=true;
+      this.playerHealth=100;
+      this.monsterHealth=100;
+      this.count=0;
+
+    },
+    onQuit:function(){
+      this.isGameStarted=false;
+      this.playerHealth=100;
+      this.monsterHealth=100;
+      this.count=0;
+    },
+    onAttack:function(){
+      let damageM=Math.max(Math.ceil(Math.random()*10),3);
+      this.monsterHealth-=damageM;
+
+
+      if(this.monsterHealth<=0){
+        alert('You Won');
+        this.isGameStarted=false;
+      }
+
+      let damageP=Math.max(Math.ceil(Math.random()*15),3);
+      this.playerHealth-=damageP;
+
+      if(this.playerHealth<=0){
+        alert('You loss');
+        this.isGameStarted=false;
+      }
+    },
+    onHeal:function(){
+      if(this.playerHealth>=100){
+        this.playerHealth=100;
+        alert('full health')
+        return;
+      }
+      if(this.count<5){
+      let health=Math.ceil(Math.random()*5);
+      this.playerHealth+=health;
+      this.count++;
+      }
+      else{
+        alert('You used all your healing powers');
+      }
+    },
+    onSpAttack:function(){
+      let damageM=Math.max(Math.ceil(Math.random()*17),5);
+      this.monsterHealth-=damageM;
+
+
+      if(this.monsterHealth<=0){
+        alert('You Won');
+        this.isGameStarted=false;
+      }
+
+      let damageP=Math.max(Math.ceil(Math.random()*13),3);
+      this.playerHealth-=damageP;
+
+      if(this.playerHealth<=0){
+        alert('You loss');
+        this.isGameStarted=false;
+      }
+    }
+
+    }
+
+
+});
